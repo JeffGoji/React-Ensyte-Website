b2ccd27a17255d451ec80706ffdd91c0eda3f6e6
JeffGoji <viperhq@hotmail.com>
2022-06-23 16:53:03 -0500

    refractored carousel slide code slightly

--- src/components/Carousel/Slide7.js
+++ src/components/Carousel/Slide7.js
@@ -6,7 +6,7 @@ export default class Slide7 extends React.Component {
 
         return (
             <div className='justify-content-center bg-white'>
-                <div className="card text-black lightGradient imgBg6 mt-0" style={{ minHeight: "700px" }}>
+                <div className="card text-black lightGradient imgBg6 mt-0" style={{ minHeight: "600px" }}>
                     <h1 className="card-title mt-5 blue-text">Summer Updates for GASTAR-LDC for Natural Gas Utilities!</h1>
                     <p className="card-text fs-3 mt-4"> The ENSYTE Team has made significant enhancements to the GASTAR-LDC Solution, the industry’s most comprehensive natural gas software for natural gas utilities, including:</p>
                     <div className='text-center fs-4 mt-5'>
