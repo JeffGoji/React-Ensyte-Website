814ccbf123fd14770820a7cb4e1d8190b02e20d3
JeffGoji <viperhq@hotmail.com>
2022-06-24 15:27:36 -0500

    Testing styling

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -5,15 +5,15 @@ const ContactForm = () => {
             <input type="hidden" name="form-name" value="contact" />
             <p>
                 <label htmlFor="name">Name</label> <br />
-                <input type="text" id="name" name="name" required />
+                <input type="text" id="name" name="name" required className="form-control" />
             </p>
             <p>
                 <label htmlFor="email">Email</label> <br />
-                <input type="email" id="email" name="email" required />
+                <input type="email" id="email" name="email" required className="form-control" />
             </p>
             <p>
                 <label htmlFor="message">Message</label> <br />
-                <textarea id="message" name="message" required></textarea>
+                <textarea id="message" name="message" required className="form-control" row="3"></textarea>
             </p>
             <p>
                 <input type="submit" value="Submit message" />
