3564e25013152ba5303c4cb97b044a4c8a4d13a9
JeffGoji <viperhq@hotmail.com>
2022-06-24 15:13:25 -0500

    testing new forms

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -1,24 +1,22 @@
 const ContactForm = () => {
     return (
 
-        <form name="contact" method="POST" data-netlify="true" action="/success">
+        <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
             <p>
-                <label>Your Name: <input type="text" name="name" className="form-control" id="exampleFormControlInput1" placeholder="John" /></label>
+                <label htmlFor="name">Name</label> <br />
+                <input type="text" id="name" name="name" required />
             </p>
             <p>
-                <label>Your Email: <input type="email" name="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" /></label>
-            </p>
-            {/* <p>
-                <label>Your Role: <select name="role[]" multiple>
-                    <option value="leader">Leader</option>
-                    <option value="follower">Follower</option>
-                </select></label>
-            </p> */}
-            <p>
-                <label>Message: <textarea name="message" className="form-control" rows="3"></textarea></label>
+                <label htmlFor="email">Email</label> <br />
+                <input type="email" id="email" name="email" required />
             </p>
             <p>
-                <button type="submit" className="btn btn-primary">Send</button>
+                <label htmlFor="message">Message</label> <br />
+                <textarea id="message" name="message" required></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Submit message" />
             </p>
         </form>
     )
