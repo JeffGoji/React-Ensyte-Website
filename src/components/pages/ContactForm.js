6f4c2affb9a6a6902c9b23cc6ff0d3fe160c7c9c
JeffGoji <viperhq@hotmail.com>
2022-06-27 14:59:52 -0500

    form testing

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -29,7 +29,7 @@ const ContactForm = () => {
                 <textarea id="message" name="message" required className="form-control" row="5"></textarea>
             </p>
             <p>
-                <input htmlFor="I am not a robot" className="form-check-input m-2" type="checkbox" value="no" id="flexCheckDefault"></input>
+                <input htmlFor="I am not a robot" className="form-check-input m-2" type="checkbox" value="true" id="I am not a robot"></input>
                 <label className="form-check-label" htmlFor="flexCheckDefault">
                     I am not a Robot
                 </label>
