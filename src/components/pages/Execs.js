62aeed94ae8d5bf7f00f605938464a4564765c42
JeffGoji <viperhq@hotmail.com>
2022-07-28 15:10:55 -0500

    restyled About Us page

--- src/components/pages/Execs.js
+++ src/components/pages/Execs.js
@@ -13,16 +13,14 @@ const Execs = () => {
     }, []);
 
     return (
-        <div className='container-fluid text-white blue-bg'>
-            <div className="row justify-content-center blue-bg">
-                <h1 className='text-white text-center mt-3 mb-3'>About Us</h1>
-            </div>
+        <div className='container-fluid'>
+
             <div className="row justify-content-center">
                 <div className="col-sm-12 col-md-12 col-lg-8">
-                    <h3 className="text-center mt-3 mb-3">
+                    <h3 className="text-center mt-3">
                         Meet Our Executive Team
                     </h3>
-                    <div className="text-white mb-5">
+                    <div className="mb-5">
                         <div className="row justify-content-center row-cols-1 row-cols-md-3 g-2 mt-5 fs-5 text-center">
                             <div className="col-sm-12 col-md-12 col-lg-4">
                                 <div className="card h-100 bg-transparent border-0">
@@ -30,7 +28,7 @@ const Execs = () => {
                                         <img src={image3} alt="Tania Demeris" className="img-fluid rounded" style={{ maxHeight: "264px" }} />
                                         <h3 className="card-title mt-2">Tania Demeris</h3>
                                         <p className="card-text">President & CEO</p>
-                                        <NavLink to="/tania-demeris" className="link-white">Learn more about Ms. Demeris</NavLink>
+                                        <NavLink to="/tania-demeris" className="link">Learn more about Ms. Demeris</NavLink>
                                     </div>
                                 </div>
                             </div>
@@ -41,7 +39,7 @@ const Execs = () => {
                                         <img src={image1} alt="Michael Smith" className="img-fluid rounded" style={{ maxHeight: "264px" }} />
                                         <h3 className="card-title mt-2">Michael Smith</h3>
                                         <p className="card-text">Director / Visionary</p>
-                                        <NavLink to="/michael-smith" className="link-white">Learn more about Mr. Smith</NavLink>
+                                        <NavLink to="/michael-smith" className="link">Learn more about Mr. Smith</NavLink>
                                     </div>
                                 </div>
                             </div>
@@ -52,7 +50,7 @@ const Execs = () => {
                                         <img src={image2} alt="Marilyn Smith" className="img-fluid rounded" style={{ maxHeight: "264px" }} />
                                         <h3 className="card-title mt-2">Marilyn Smith</h3>
                                         <p>CFO</p>
-                                        <NavLink to="/marilyn-smith" className="link-white">Learn more about Mrs. Smith</NavLink>
+                                        <NavLink to="/marilyn-smith" className="link">Learn more about Mrs. Smith</NavLink>
                                     </div>
                                 </div>
                             </div>
